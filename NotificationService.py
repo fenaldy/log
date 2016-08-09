a4c6a125df59fc92781429e93aaf06a7bb81cc14
Gibransyah Fakhri <gibransyah@gmail.com>
2016-08-09 16:17:16 +0700

    Enchancement again

---
+++ NotificationService.py
@@ -0,0 +1,8 @@
+				mail.sendemail(from_addr    = 'fenaldy12@gmail.com', 
+        				to_addr_list = ['fenaldy13@gmail.com'],
+        				cc_addr_list = ['example@gmail.com'], 
+        				subject      = 'Pesan Error !', 
+        				message      =  msg, 
+        				login        = 'fenaldy12@gmail.com', 
+        				password     = 'mefor345')
+	
---
+++ dbutil.py
@@ -0,0 +1,35 @@
+
+import sys
+import psycopg2
+from datetime import datetime
+
+class dbutil(object):
+    
+    conn = None
+    cursor = None
+    
+    def __init__(self): 
+        self.connect() 
+        print "A"
+
+    def connect(self):
+        db = "hrisok"
+        username = "hris"
+        password = "fl4tr0n!"
+        host = "dev"
+        port = "5433"
+        self.conn = psycopg2.connect(database=db, user=username, 
+                    password=password, host=host, port=port)
+        self.conn.set_session(isolation_level=None, readonly=None, deferrable=None, autocommit=True)
+
+        self.cursor = self.conn.cursor()
+
+    def query(self, sql):
+        try:
+            self.cursor.execute(sql) 
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+    def commit(self):
+        self.conn.commit()
