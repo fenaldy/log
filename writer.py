9b1f8e235536bf4d9ea15a4bdb6a0737fbc97be2
Gibransyah Fakhri <gibransyah@gmail.com>
2016-08-09 16:16:39 +0700

    Enhancement

--- writer.py
+++ writer.py
@@ -5,11 +5,25 @@ import subprocess
 from datetime import datetime
 from time import sleep
 
-logTag = ['ERR','DEBUG','INFO']
+log_tag = ['DEBUG','INFO']
+log_speed = 1 # in seconds
+counter = 0
+counter_modulus = 5 # raise error exactly at n iteration
+
+with open('err_template.txt','r') as f:
+    err_template = f.read()
 
 while True:
-    currentEvent = random.choice(logTag)
-    now = str(datetime.now())
+    if (counter % counter_modulus == 0):
+        current_event = 'ERR'
+        msg = err_template  
+    else:
+        current_event = random.choice(log_tag)
+        msg = 'pesan kesalahan...'  
+
+    now = datetime.now()
+    event_time = now.strftime("%Y-%m-%dT%H:%M:%S")
     sys.stdout = open('app.log','a')
-    print now + ' ' + currentEvent + ' : sSekarang sedang ' + currentEvent +' pesan kesalahan'
-    sleep(5)
+    print event_time + '+07:00 ' + current_event + ' (7): ' + msg
+    counter = counter + 1
+    sleep(log_speed)
