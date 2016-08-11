9d11640dc9283f9f4b7c6a4e11d90a1ca0e30c67
fenaldy <fenaldy13@gmail.com>
2016-08-11 16:38:55 +0700

    use smptlib

--- NotificationService.py
+++ NotificationService.py
@@ -1,37 +1,33 @@
-
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
-from email import encoders
- 
-fromaddr = ""
-toaddr = "fenaldy@gmail.com"
- 
-msg = MIMEMultipart()
- 
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "SUBJECT OF THE EMAIL"
- 
-body = "TEXT YOU WANT TO SEND"
- 
-msg.attach(MIMEText(body, 'plain'))
- 
-filename = "NAME OF THE FILE WITH ITS EXTENSION"
-attachment = open("./docs/err_template.txt", "rb")
- 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
- 
-msg.attach(part)
- 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(fromaddr, "put_your_password_here")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+
+class NotificationService(object):
+
+  def init(self, from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver, smtpport):
+    '''inisialisasi parameter'''
+    self.from_addr = from_addr
+    self.to_addr_list = to_addr_list
+    self.cc_addr_list = cc_addr_list
+    self.subject = subject
+    self.message = message
+    self.login = login
+    self.password = password
+    self.smtpserver = smtpserver
+    self.smtpport = smtpport
+
+    #NotificationService.sender() #memanggil fungsi
+
+  def sender(self):  # split smtpserver and -port
+      try:
+        self.header  = 'From: %s\n' % self.from_addr
+        self.header += 'To: %s\n' % ','.join(self.to_addr_list)
+        self.header += 'Cc: %s\n' % ','.join(self.cc_addr_list)
+        self.header += 'Subject: %s\n\n' % self.subject
+        self.message = self.header + self.message
+      except:
+        return "attribut belum terisi !"
+        
+      server = smtplib.SMTP(self.smtpserver, self.smtpport)  # use both smtpserver  and -port 
+      server.starttls()
+      server.login(self.login,self.password)
+      problems = server.sendmail(self.from_addr, self.to_addr_list, self.message)
+      server.quit()
