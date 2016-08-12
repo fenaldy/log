8636bc9f0c1d9bc5e8521c7a15528a81f687cac5
fenaldy <fenaldy13@gmail.com>
2016-08-12 16:28:09 +0700

    mailer

--- NotificationService.py
+++ NotificationService.py
@@ -1,28 +1,34 @@
 import smtplib
 
 class NotificationService(object):
+  from_addr = 'fenaldy12@gmail.com'
+  to_addr_list = ['fenaldy13@gmail.com']
+  cc_addr_list = ['example@gmail.com']
+  subject = 'Pesan Error !'
+  login = 'fenaldy12@gmail.com'
+  password = 'thez4rk0m'
+  smtpserver = 'smtp.gmail.com'
+  smtpport = 587
 
-  def init(self, from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver, smtpport):
-    '''inisialisasi parameter'''
-    self.from_addr = from_addr
-    self.to_addr_list = to_addr_list
-    self.cc_addr_list = cc_addr_list
-    self.subject = subject
-    self.message = message
-    self.login = login
-    self.password = password
-    self.smtpserver = smtpserver
-    self.smtpport = smtpport
+  def __init__(self):
+    self.from_addr
+    self.to_addr_list
+    self.cc_addr_list
+    self.subject
+    self.login
+    self.password
+    self.smtpserver
+    self.smtpport
+  
 
-    #NotificationService.sender() #memanggil fungsi
+  def send(self, msg):  # split smtpserver and -port
 
-  def sender(self):  # split smtpserver and -port
       try:
         self.header  = 'From: %s\n' % self.from_addr
         self.header += 'To: %s\n' % ','.join(self.to_addr_list)
         self.header += 'Cc: %s\n' % ','.join(self.cc_addr_list)
         self.header += 'Subject: %s\n\n' % self.subject
-        self.message = self.header + self.message
+        self.message = self.header + msg
       except:
         return "attribut belum terisi !"
         
